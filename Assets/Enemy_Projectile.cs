c6f10a7c7563bb7283bc80207101d08f7591ab10
SirFilipVonHabsburg <filip.boboli@gmail.com>
2025-12-09 22:46:15 +0100

    Enemy projectile first part
    started doing a part of the script but not all

--- Assets/Enemy_Projectile.cs
+++ Assets/Enemy_Projectile.cs
@@ -8,12 +8,20 @@ public class Enemy_Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag== "Boundary")
+        {
+            Destroy(gameObject);
+        }
     }
 }
